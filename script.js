bb2ddcf391903832dce1be086483f4f32b3f65f1
timothyhoover <timmchoover@gmail.com>
2021-01-21 13:22:57 -0600

    add questions.js and layout for script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Starting Comments and Setup 
+
+Functionality Layout
+
+- Start container shows up first and user can click button to start quiz.
+- Check button shows up before the next question button
+- When user selects answer, the option container highlights red for false or green for correct
+    * The x and check icons need to be in white and only pop when user selects the check button
+    * The user should not be able to click on any other answer
+    * The background color for the prefix circle needs to first be filled with #a9a9a9 to confirm the answer has been selected.
+    * The background color and border for the wrong answer should be #b80000.
+    * The background color and border for the correct answer should be #62dbc8.
+- After user has clicked on the check button the next button should appear.
+- The number of questions should dynamically change.
+- When there are no more questions left, the end container should appear with the user's results
+
+JS todo list: 
+1. Create numbers, questions, answers and correct answers object
+2. Bring in all necessary elements from HTML using DOM selectors
+
+
+*/
